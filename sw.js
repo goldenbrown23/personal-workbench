187e5c3ca2016dbab77e75a5e56fcdcbad09465e
goldenbrown23 <semoneskateyu1@gmail.com>
2026-08-31 20:23:51 -0500

    bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "personal-workbench-v5";
+const CACHE_NAME = "personal-workbench-v6";
 const APP_SHELL = [
   "./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png",
   "./styles.css",
